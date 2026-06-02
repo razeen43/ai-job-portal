be41b3b0fe33a1ef147248578bdba15fce4ee462
razeen43 <razc571@gmail.com>
2026-06-02 17:18:55 +0530

    Use MySQL connection pool

--- server/db.js
+++ server/db.js
@@ -1,19 +1,17 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-db.connect((err) => {
-    if (err) {
-        console.log("Database Error:", err);
-    } else {
-        console.log("MySQL Connected");
-    }
-});
+console.log("MySQL Pool Created");
 
 module.exports = db;
